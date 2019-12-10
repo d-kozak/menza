a6543e5bce957172aa91ecdf8f512d0d32f1cb58
dkozak <dkozak94@gmail.com>
2019-12-10 10:37:59 +0100

    add cors config

---
+++ src/main/kotlin/io/dkozak/menza/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package io.dkozak.menza.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowCredentials(true)
+            }
+        }
+    }
+}
